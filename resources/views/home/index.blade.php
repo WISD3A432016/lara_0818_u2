f397b3c07d7bfdcbb2026a4cccb882452ecd95ea
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-18 16:51:16 +0800

    Exercise 2-2-b edit index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<h1>Home</h1>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('title','Home')
+
+@section('content')
+    <h1>Home</h1>
+    <p>This is my body content.</p>
+@endsection
